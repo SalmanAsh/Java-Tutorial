ad5ae1967a932240e0ef2dcd9412d8ec4c6e1df0
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2022-08-18 12:13:57 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.sql.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        // primitive
+        int age = 30;
+        double num = 30.0;
+        boolean b = true;
+        char c = 'o';
+
+        // Reference
+        String str = "Hello, how are you?";
+        System.out.println(str);
+        System.out.println(b);
+        System.out.println(age);
+
+        int tim = age;
+
+    }
+}
